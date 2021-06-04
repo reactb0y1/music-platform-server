a163464e08356a0d2cabe74a03276a50860f8256
Alexandr <alex.develop777@gmail.com>
2021-06-04 18:57:23 +0300

    track.service.ts create(); create-track.dto.ts

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,12 +1,21 @@
 import {Injectable} from "@nestjs/common";
+import {InjectModel} from '@nestjs/mongoose';
+import {Track, TrackDocument} from "./schemas/track.schema";
+import {Model} from "mongoose";
+import {CommentDocument} from "./schemas/comment.schema";
+import {CreateTrackDto} from "./dto/create-track.dto";
 
 
 @Injectable()
 export class TrackService {
 
-    // Для создания трека
-    async create() {
+    constructor(@InjectModel(Track.name) private trackModel: Model<TrackDocument>,
+                @InjectModel(Comment.name) private commentModel: Model<CommentDocument>) {}
 
+    // Для создания трека
+    async create(dto: CreateTrackDto): Promise<Track> {
+        const track = await this.trackModel.create({...dto, listens: 0});
+        return track
     }
 
     // Получить все треки из БД
