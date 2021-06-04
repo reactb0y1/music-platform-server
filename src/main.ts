1b4d6ed9fa24fdd12f8cfe8fe4c851656faefe74
Alexandr <alex.develop777@gmail.com>
2021-06-04 15:43:49 +0300

    server/src/main.ts: PORT, app.listen; start:dev

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,13 @@ import {AppModule} from "./app.module";
 
 const start = async () => {
     try {
+        const PORT = process.env.PORT || 5000;
         const app = await NestFactory.create(AppModule);
+
+        await app.listen(PORT, () => console.log(`server started on PORT ${PORT}`))
     } catch (e) {
         console.log(e);
     }
 };
+
+start();
