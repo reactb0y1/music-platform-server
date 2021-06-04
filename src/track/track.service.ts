70b97b8cd353efd748903abe8bb15c45239fb4ff
Alexandr <alex.develop777@gmail.com>
2021-06-04 16:45:43 +0300

    track.module.ts, track.controller.ts, track.service.ts:
    connect each with other

---
+++ src/track/track.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from "@nestjs/common";
+
+
+@Injectable()
+export class TrackService {
+
+    // Для создания трека
+    async create() {
+
+    }
+
+    // Получить все треки из БД
+    async getAll() {
+
+    }
+
+    // Получить один трек
+    async getOne() {
+
+    }
+
+    // Удалить трек
+    async delete() {
+
+    }
+
+}
