e7a375033217a4776fb84201c717034fc0c88e80
Alexandr <alex.develop777@gmail.com>
2021-06-04 18:31:11 +0300

    app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import {Module} from "@nestjs/common";
 import {TrackModule} from "./track/track.module";
+import {MongooseModule} from "@nestjs/mongoose";
 
 @Module({
-    imports: [TrackModule]
+    imports: [
+        MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.kgrlw.mongodb.net/music-platform?retryWrites=true&w=majority'),
+        TrackModule
+    ]
 })
 
 export class AppModule {}
