dca22e2303542bf70449f0380fb428fa71bd8443
Alexandr <alex.develop777@gmail.com>
2021-06-04 15:57:56 +0300

    server/src/app.service.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,8 @@ import {Module} from "@nestjs/common";
 import {AppController} from "./app.controller";
 
 @Module({
-    controllers: [AppController]
+    controllers: [AppController],
+    providers: [AppController]
 })
 
 export class AppModule {}
