7cab4ac67188ce2bc6c67ba6de2c9cf097627283
Alexandr <alex.develop777@gmail.com>
2021-06-05 11:34:24 +0300

    file: createFile() load in file system

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import {Module} from "@nestjs/common";
 import {TrackModule} from "./track/track.module";
 import {MongooseModule} from "@nestjs/mongoose";
+import {FileModule} from "./file/file.module";
 
 @Module({
     imports: [
         MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.kgrlw.mongodb.net/music-platform?retryWrites=true&w=majority'),
-        TrackModule
+        TrackModule,
+        FileModule
     ]
 })
 
--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -1,8 +1,9 @@
 import {Module} from "@nestjs/common";
+import {FileService} from "./file.service";
 
 @Module({
     
 })
 export class FileModule {
-    
+    providers: [FileService]
 }
