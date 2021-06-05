62aa5af4b430bcad97a9f6ad51161cdec43d3b4a
Alexandr <alex.develop777@gmail.com>
2021-06-05 09:37:38 +0300

    track: addComment()

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -2,6 +2,7 @@ import {Body, Controller, Delete, Get, Param, Post} from "@nestjs/common";
 import {TrackService} from "./track.service";
 import {CreateTrackDto} from "./dto/create-track.dto";
 import {ObjectId} from "mongoose";
+import {CreateCommentDto} from "./dto/create-comment.dto";
 
 
 @Controller('/tracks')
@@ -26,6 +27,11 @@ export class TrackController {
     @Delete(':id')
     delete(@Param('id') id: ObjectId) {
         return this.trackService.delete(id)
+    }
+
+    @Post('/comment')
+    addComment(@Body() dto: CreateCommentDto) {
+        return this.trackService.addComment(dto);
     }
 
 }
--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -4,6 +4,7 @@ import {Track, TrackDocument} from "./schemas/track.schema";
 import {Model, ObjectId} from "mongoose";
 import {Comment, CommentDocument} from "./schemas/comment.schema";
 import {CreateTrackDto} from "./dto/create-track.dto";
+import {CreateCommentDto} from "./dto/create-comment.dto";
 
 
 @Injectable()
@@ -34,6 +35,14 @@ export class TrackService {
     async delete(id: ObjectId): Promise<ObjectId> {
         const track = await this.trackModel.findByIdAndDelete(id);
         return track._id
+    }
+
+    async addComment(dto: CreateCommentDto): Promise<Comment> {
+        const track = await this.trackModel.findById(dto.trackId);
+        const comment = await this.commentModel.create({...dto});
+        track.comments.push(comment._id);
+        await track.save();
+        return comment
     }
 
 }
